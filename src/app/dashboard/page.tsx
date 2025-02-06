10bcf34620639ab7be498aea270ac3c43cf68c3c
momadAB <mabaqer99@gmail.com>
2025-02-06 18:04:49 +0300

    feat: one on one messaging service on dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Chat from "../../components/notifications";
 
 const page = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Chat></Chat>
+    </div>
+  );
 };
 
 export default page;
