b2d1b44c5a95a46946332f4f8aeb754cedfdbcc9
ibrahim.a.y.m.98@gmail.com <ibrahim.a.y.m.98@gmail.com>
2025-02-18 10:07:04 +0300

    change variable name

--- src/app/api/actions/downloadFile.tsx
+++ src/app/api/actions/downloadFile.tsx
@@ -3,12 +3,12 @@ import { axiosInstance } from "./config";
 
 export async function downloadPDF(
   fileId: string,
-  userId: string
+  documentName: string
 ): Promise<void> {
   try {
     // You can now pass the additional parameter (userId) to the API request
     const response = await axiosInstance.get(`/api/files/${fileId}`, {
-      params: { userId }, // Pass userId as a query parameter (you can adjust this as needed)
+      params: { documentName }, // Pass userId as a query parameter (you can adjust this as needed)
       responseType: "blob", // Set responseType to 'blob' for file download
     });
 
@@ -16,7 +16,7 @@ export async function downloadPDF(
     const link = document.createElement("a");
     const fileURL = window.URL.createObjectURL(new Blob([response.data]));
     link.href = fileURL;
-    link.download = `${userId}_${fileId}.pdf`; // Name the file, including userId in filename
+    link.download = `${documentName}.pdf`; // Name the file, including userId in filename
     link.click(); // Trigger download
 
     // Clean up the URL object after the download starts
